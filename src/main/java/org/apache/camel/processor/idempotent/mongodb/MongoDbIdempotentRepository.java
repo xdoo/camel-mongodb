fc34040ab8d9841498b5a5a99623bf64c7bfbfb1
Claus Straube <info@catify.com>
2010-11-25 18:15:07 +0100

    changed package due to OSGi problems

--- src/main/java/org/apache/camel/processor/idempotent/mongodb/MongoDbIdempotentRepository.java
+++ src/main/java/org/apache/camel/processor/idempotent/mongodb/MongoDbIdempotentRepository.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.camel.processor.idempotent;
+package org.apache.camel.processor.idempotent.mongodb;
 
 import java.net.UnknownHostException;
 import java.util.Date;
